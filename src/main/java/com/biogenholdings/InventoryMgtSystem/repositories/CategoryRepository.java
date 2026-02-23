496015a6a7095335220f159244c4d0d2a9a2e564
MWijesekara <menurawijesekara17@gmail.com>
2026-02-23 11:23:18 +0530

    Category service soft delete API implementation and fetch data update

--- src/main/java/com/biogenholdings/InventoryMgtSystem/repositories/CategoryRepository.java
+++ src/main/java/com/biogenholdings/InventoryMgtSystem/repositories/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.biogenholdings.InventoryMgtSystem.repositories;
 
 import com.biogenholdings.InventoryMgtSystem.models.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.List;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     List<Category> findByNameContainingOrDescriptionContaining(String name, String description);
+    List<Category> findByisDeletedFalse();
+    Page<Category> findByIsDeletedFalse(Pageable pageable);
+
 }
