a379fcb34fdbacf2a83cd9652e7f81a83eaa1bbd
MWijesekara <menurawijesekara17@gmail.com>
2026-03-31 13:12:44 +0530

    Chat service and reports

--- src/main/java/com/biogenholdings/InventoryMgtSystem/services/ChatService.java
+++ src/main/java/com/biogenholdings/InventoryMgtSystem/services/ChatService.java
@@ -1,5 +1,5 @@
 package com.biogenholdings.InventoryMgtSystem.services;
 
 public interface ChatService {
-    public Object process(String message);
+    Object process(String message);
 }
--- src/main/java/com/biogenholdings/InventoryMgtSystem/services/GeminiService.java
+++
@@ -1,6 +0,0 @@
-package com.biogenholdings.InventoryMgtSystem.services;
-
-public interface GeminiService {
-
-    public String askGemini(String userMessage);
-}
